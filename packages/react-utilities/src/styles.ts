166336cad84940dbbd60a9b17e62035616a1119f
Tzvi Melamed <tzvi@melamed.biz>
2018-03-16 08:59:42 -0400

    Set up react-utilities

--- packages/react-utilities/src/styles.ts
+++ packages/react-utilities/src/styles.ts
@@ -1,6 +1,6 @@
 import classnames = require('classnames'); // tslint:disable-line no-require-imports
 
-export const classNames = classnames;
+export const classNames = classnames as any;
 
 export function variationName(name: string, value: string | number) {
   const valuePortion = typeof value === 'number' ? String(value) : `${value[0].toUpperCase()}${value.substring(1)}`;
